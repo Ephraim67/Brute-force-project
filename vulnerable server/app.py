8d571ed643bd1a7809c44da79b555b40619d6c29
EL-sec <itsimmah@gmail.com>
2025-10-04 10:53:59 +0100

    flask library importation & settingup flask app to access route path

--- vulnerable server/app.py
+++ vulnerable server/app.py
@@ -7,7 +7,7 @@ from flask import Flask
 # the line tells python to import the flask class from the library of the already installed framework (flask)
 # this should server as the Web Server Gateway Interface
 
-Project = Flask(__name__)
+app = Flask(__name__)
 
 # Project here is a variable that has been declared to define the site,
 # Flask is the framework where the resource (_name_) is derived from. 
@@ -15,7 +15,7 @@ Project = Flask(__name__)
 
 # setup flask app to access route path
 
-@Project.route('/')
+@app.route('/')
 
 # this line tells the framework what to do when someone visits "/" (the homepage).
 
@@ -23,7 +23,7 @@ def home():
     return 'Welcome to my page Mr Ephraim!'
 
 if __name__ == '__main__':
-    Project.run(debug=True)
+    app.run(debug=True)
 
 # it would be Running on http://127.0.0.1:5000 becuase it's built to run via that route on the framework (flask)
 
