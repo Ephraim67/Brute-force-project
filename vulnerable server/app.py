477b9627efdcb450323f5683b553d559ae98a620
Ephraim67 <jonathaneuphrem@gmail.com>
2025-10-04 12:26:31 +0100

    Added more things

--- vulnerable server/app.py
+++ vulnerable server/app.py
@@ -13,19 +13,28 @@ app.config['MYSQL_DB']  = '______________'
 
 mysql = MySQL(app)
 
+# home route
 @app.route('/')
+
+# login route
+# define login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
     
     return render_template('login.html', msg=msg)
 
+# logout route
+# define logout route
 @app.route('/logout')
 def logout():
     
     return redirect(url_for('login'))
 
+# register route
+# define register route
 @app.route('/register', methods=['GET', 'POST'])
+
 def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
